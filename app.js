dc1c76b79fb56b989a921f942f0a6f4ddce358c1
miqelle <miqellec3@gmail.com>
2021-07-23 12:47:37 -0500

    code along from 723 class

---
+++ app.js
@@ -0,0 +1,40 @@
+//Enter each item they have for sale. Go through each item and print them in console. 
+//Print them on the webpage
+/* let custItems = []
+function listItems () {
+prompt("What items would you like to list today?");
+    console.log();
+}
+listItems();
+let button = document.querySelector('button');
+button.addEventListener('click', listItems); 
+ */
+
+
+
+
+
+
+//array list to hold user items
+let customerItems = [];
+
+//user done entering items
+let doneListing = true;
+
+//track number of items
+let itemCount = 1;
+
+while (doneListing == false) {
+    let userQuestion = prompt("What items would you like to list today?");
+    if (userQuestion === "done") {
+        doneListing = false;
+    }
+    if (customerItems) {
+document.
+    }
+}
+
+
+
+
+
